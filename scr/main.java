189ba2b7d810524a73c3b63fd2b7892913f62afe
Chiyo Mochizuki <Chiyo Mochizuki@DESKTOP-L1VT2TP>
2023-06-23 07:44:48 +0300

    Read3

--- scr/main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int TicketPriceKpks;
-        TicketPriceKpks = 20_000_00;
-        int bonusMiles = TicketPriceKpks / 20 / 100;
-        System.out.println(bonusMiles);
-    }
-}
